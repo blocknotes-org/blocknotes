25e3490e1cfb5a1681f216f7fabcbbbbd3fdfd6c
Ella van Durpe <ella@vandurpe.com>
2023-06-12 14:00:30 +0300

    filesystem

--- src/js/capacitor-welcome.js
+++ src/js/capacitor-welcome.js
@@ -1,75 +1,72 @@
 import { startPlaygroundWeb } from './client.js';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
 async function onClick() {
-//   const directoryHandle = await showDirectoryPicker( {
-//     mode: 'readwrite',
-//   } );
+  const posts = {};
 
-//   if (!directoryHandle) return;
+  const dir = await Filesystem.readdir({
+    path: '',
+    directory: Directory.Documents,
+  });
 
-//   const posts = {};
+  for ( const file of dir.files ) {
+    if ( file.name.endsWith( '.html' ) ) {
+      console.log(file)
+      const text = await Filesystem.readFile({
+        path: file.name,
+        directory: Directory.Documents,
+        encoding: Encoding.UTF8,
+      });
+      posts[ file.name.replace( /\.html$/i, '' ) ] = text.data.replace( "'", "\\'" );
+    }
+  }
 
-//   for await (const entry of directoryHandle.values()) {
-//     if (entry.kind === "file" && entry.name.endsWith( '.html' ) ){
-//       const file = await entry.getFile();
-//       const text = await file.text();
-//       posts[ entry.name.replace( /\.html$/i, '' ) ] = text.replace( "'", "\\'" );
-//     }
-//   }
+  let insert = '';
 
-//   let insert = '';
+  for  ( const key in posts ) {
+    insert += `
+      echo wp_insert_post( [
+        'post_type' => 'hypernote',
+        'post_title' => '${key}',
+        'post_content' => '${posts[key]}',
+        'post_status' => 'publish',
+        'post_author' => 1,
+      ] );
+    `;
+  }
 
-//   for  ( const key in posts ) {
-//     insert += `
-//       echo wp_insert_post( [
-//         'post_type' => 'hypernote',
-//         'post_title' => '${key}',
-//         'post_content' => '${posts[key]}',
-//         'post_status' => 'publish',
-//         'post_author' => 1,
-//       ] );
-//     `;
-//   }
+  const PHP = `<?php
+    include 'wordpress/wp-load.php';
+    ${insert}
+  `;
+  // console.log(posts)
+  const plugin = `<?php
+    add_filter( 'wp_insert_post_data', function( $data ) {
+      if ( $data['post_type'] !== 'hypernote' ) return $data;
+      if ( $data['post_status'] !== 'private' ) return $data;
+      $blocks = parse_blocks( $data['post_content'] );
 
-//   const PHP = `<?php
-//     include 'wordpress/wp-load.php';
-//     ${insert}
-//   `;
-//   console.log(posts)
-//   const plugin = `<?php
-//     add_filter( 'wp_insert_post_data', function( $data ) {
-//       if ( $data['post_type'] !== 'hypernote' ) return $data;
-//       if ( $data['post_status'] !== 'private' ) return $data;
-//       $blocks = parse_blocks( $data['post_content'] );
+      $i = 0;
+      $text = '';
 
-//       $i = 0;
-//       $text = '';
+      while ( isset( $blocks[ $i ] ) ) {
+        $text = wp_trim_words( $blocks[ $i ]['innerHTML'], 10, '' );
+        $i++;
 
-//       while ( isset( $blocks[ $i ] ) ) {
-//         $text = wp_trim_words( $blocks[ $i ]['innerHTML'], 10, '' );
-//         $i++;
-
-//         if ( $text ) {
-//           break;
-//         }
-//       }
-//       if ( ! $text ) return $data;
-//       post_message_to_js( json_encode( array(
-//         'name' => $data['post_title'],
-//         'newName' => $text,
-//         'content' => $data['post_content'],
-//       ) ) );
-//       $data['post_title'] = $text;
-//       return $data;
-//     } );
-//   `;
-
-  // register a service worker
-  // if ('serviceWorker' in navigator) {
-  //   console.log('Registering service worker...');
-  //   const ret = await navigator.serviceWorker.register('/sw.js');
-  //   console.log('Service worker registered.', ret);
-  // }
+        if ( $text ) {
+          break;
+        }
+      }
+      if ( ! $text ) return $data;
+      post_message_to_js( json_encode( array(
+        'name' => $data['post_title'],
+        'newName' => $text,
+        'content' => $data['post_content'],
+      ) ) );
+      $data['post_title'] = $text;
+      return $data;
+    } );
+  `;
 
   console.log('starting playground...')
 
@@ -97,38 +94,35 @@ async function onClick() {
             slug: 'hypernotes',
           },
         },
-        // {
-        //   step: 'runPHP',
-        //   code: PHP,
-        // },
-        // {
-        //   step: 'writeFile',
-        //   path: 'wordpress/wp-content/mu-plugins/test.php',
-        //   data: plugin,
-        // }
+        {
+          step: 'runPHP',
+          code: PHP,
+        },
+        {
+          step: 'writeFile',
+          path: 'wordpress/wp-content/mu-plugins/test.php',
+          data: plugin,
+        }
       ],
     },
   });
 
-  // wp.src = "http://localhost:3000/hello";
+  client.onMessage( async ( data ) => {
+    const { name, content, newName } = JSON.parse( data );
+  
+    await Filesystem.writeFile({
+      path: name + '.html',
+      data: content,
+      directory: Directory.Documents,
+      encoding: Encoding.UTF8,
+    });
+    await Filesystem.rename({
+      from: name + '.html',
+      to: newName + '.html',
+      directory: Directory.Documents,
+    });
+  } )
 
-  // client.onMessage( async ( data ) => {
-  //   const { name, content, newName } = JSON.parse( data );
-  //   console.log( newName );
-  //   const fileHandle = await directoryHandle.getFileHandle( name + '.html', { create: true } );
-  //   const writable = await fileHandle.createWritable();
-  //   await writable.write( content );
-  //   await writable.close();
-  //   await fileHandle.move( newName + '.html' );
-  // } )
-
-  // const result = await client.run({
-  // 	code: PHP,
-  // });
-
-  // console.log( result.text )
 }
 
 onClick();
-
-// button.addEventListener('click', onClick);
