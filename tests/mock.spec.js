f02c8ea56fd364c5514b484ddb5715ee444359cb
Ella <ella@vandurpe.com>
2024-06-07 14:08:01 +0300

    Add tests folder

--- tests/mock.spec.js
+++ tests/mock.spec.js
@@ -1,27 +1,36 @@
-const { test, expect } = require('@playwright/test')
-const fs = require('fs')
-const path = require('path')
+const { test, expect } = require('@playwright/test');
+const fs = require('fs');
+const path = require('path');
 
 test('has title', async ({ page }) => {
-  await page.goto('/')
+	await page.goto('/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Blocknotes/)
-})
+	// Expect a title "to contain" a substring.
+	await expect(page).toHaveTitle(/Blocknotes/);
+});
 
 test('open file picker', async ({ page }) => {
-  const fsaMockPath = path.resolve('node_modules', 'fsa-mock', 'dist', 'fsa-mock.umd.cjs')
-  const fsaMockScript = fs.readFileSync(fsaMockPath, 'utf8')
+	const fsaMockPath = path.resolve(
+		'node_modules',
+		'fsa-mock',
+		'dist',
+		'fsa-mock.umd.cjs'
+	);
+	const fsaMockScript = fs.readFileSync(fsaMockPath, 'utf8');
 
-  await page.addInitScript(fsaMockScript)
-  await page.addInitScript(() => {
-    const { mock } = window.fsaMock
-    mock.install()
-  })
+	await page.addInitScript(fsaMockScript);
+	await page.addInitScript(() => {
+		const { mock } = window.fsaMock;
+		mock.install();
+	});
 
-  await page.goto('/')
+	await page.goto('/');
 
-  await page.getByRole('button', { name: 'Pick Folder' }).click()
+	await page.getByRole('button', { name: 'Pick Folder' }).click();
 
-  await expect(page.frameLocator('[name="editor-canvas"]').getByRole('document', { name: 'Empty block' })).toBeFocused()
-})
+	await expect(
+		page
+			.frameLocator('[name="editor-canvas"]')
+			.getByRole('document', { name: 'Empty block' })
+	).toBeFocused();
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
-  root: './src',
-  assetsInclude: ['**/*.wasm', '*.data'],
-  build: {
-    outDir: '../dist',
-    minify: false,
-    emptyOutDir: true,
-    assetsInlineLimit: 0,
-  },
-  worker: {
+	base: './',
+	root: './src',
+	assetsInclude: ['**/*.wasm', '*.data'],
+	build: {
+		outDir: '../dist',
+		minify: false,
+		emptyOutDir: true,
+		assetsInlineLimit: 0,
+	},
+	worker: {
 		format: 'es',
 	},
-  publicDir: '../public',
+	publicDir: '../public',
 });
