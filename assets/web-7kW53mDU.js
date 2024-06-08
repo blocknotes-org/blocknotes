c6ead4dc550830c5bb6c5973354c457d4b73c364
ellatrix <ellatrix@users.noreply.github.com>
2024-06-08 05:55:42 +0000

    deploy: b5614b065064042fbc58716585c18f9d194de574

---
+++ assets/web-7kW53mDU.js
@@ -0,0 +1,36 @@
+import { W as WebPlugin } from "./index-B6D6iMBt.js";
+class AppWeb extends WebPlugin {
+  constructor() {
+    super();
+    this.handleVisibilityChange = () => {
+      const data = {
+        isActive: document.hidden !== true
+      };
+      this.notifyListeners("appStateChange", data);
+      if (document.hidden) {
+        this.notifyListeners("pause", null);
+      } else {
+        this.notifyListeners("resume", null);
+      }
+    };
+    document.addEventListener("visibilitychange", this.handleVisibilityChange, false);
+  }
+  exitApp() {
+    throw this.unimplemented("Not implemented on web.");
+  }
+  async getInfo() {
+    throw this.unimplemented("Not implemented on web.");
+  }
+  async getLaunchUrl() {
+    return { url: "" };
+  }
+  async getState() {
+    return { isActive: document.hidden !== true };
+  }
+  async minimizeApp() {
+    throw this.unimplemented("Not implemented on web.");
+  }
+}
+export {
+  AppWeb
+};
