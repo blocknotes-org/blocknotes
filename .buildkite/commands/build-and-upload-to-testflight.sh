c6a87a40bbdcd0013830394f112db0b373939873
Gio Lodi <giovanni.lodi42@gmail.com>
2024-06-20 14:37:23 +1000

    Add Mac Catalyst build and TestFlight upload in CI (#14)
    * Add `.rubocop.yml`
    
    * Extract lane to only build iOS target for App Store
    
    Useful for testing locally, and might be eventually be used to split
    build from upload in CI.
    
    * Update `.editorconfig` to account for Ruby files
    
    * Add checks for required env vars in Fastlane
    
    Helps running the lanes locally for testing.
    
    * Use `npm run sync` in CI – Equivalent but a single command
    
    * Extract options from code signing and building to share later on
    
    We'll share these with the upcoming automation for the Mac Catalyst
    target.
    
    * Make automation build Mac Catalyst app together with iOS
    
    Done in the same step so that we can keep the build number consistent,
    given it's set by reading TestFlight. Otherwise, we might risk it
    diverging, which wouldn't be a drama but also would make it harder to
    identify matching builds.
    
    * Set provisioning profile for Mac Catalyst (via Xcode UI)
    
    * Also get 3rd Party Mac Developer Installer when setting up Mac Catalyst
    
    * Upload both `.ipa` and `.pkg` to TestFlight
    
    * Add pkg as a CI output
    
    * Remove branch filter, let CI config deal with this requirement
    
    * Explicitly set `app_platform` to upload both ipa and pkg
    
    * Remove unused method
    
    * Update fastlane/Fastfile
    
    Co-authored-by: Spencer Transier <17955542+spencertransier@users.noreply.github.com>
    
    * Update fastlane/lib/helpers.rb
    
    Co-authored-by: Spencer Transier <17955542+spencertransier@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ella <4710635+ellatrix@users.noreply.github.com>
    Co-authored-by: Spencer Transier <17955542+spencertransier@users.noreply.github.com>

--- .buildkite/commands/build-and-upload-to-testflight.sh
+++ .buildkite/commands/build-and-upload-to-testflight.sh
@@ -5,8 +5,7 @@ install_gems
 
 echo "--- :node: Installing NPM Dependencies and Syncing Project"
 npm ci
-npm run build
-npx cap sync
+npm run sync
 
 echo "--- :testflight: Build and upload to TestFlight"
 bundle exec fastlane build_and_upload_to_testflight
---
+++ fastlane/lib/helpers.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+ASC_API_KEY_ENV_VARS = %w[
+  APP_STORE_CONNECT_API_KEY_KEY_ID
+  APP_STORE_CONNECT_API_KEY_ISSUER_ID
+  APP_STORE_CONNECT_API_KEY_KEY
+].freeze
+
+# Fastlane match also uses MATCH_PASSWORD, but we don't expect it to be set because when running locally folks may want to use the password-in-keychain workflow.
+MATCH_ENV_VARS = %w[
+  MATCH_S3_ACCESS_KEY
+  MATCH_S3_SECRET_ACCESS_KEY
+].freeze
+
+# Use this to ensure all env vars a lane requires are set.
+#
+# The best place to call this is at the start of a lane, to fail early.
+def require_env_vars!(*keys)
+  keys.each { |key| get_required_env!(key) }
+end
+
+# Use this instead of getting values from `ENV` directly. It will throw an error if the requested value is missing.
+def get_required_env!(key, env_file_path: USER_ENV_FILE_PATH)
+  return ENV.fetch(key) if ENV.key?(key)
+
+  message = "Environment variable '#{key}' is not set."
+
+  if is_ci
+    UI.user_error!(message)
+  elsif File.exist?(env_file_path)
+    UI.user_error!("#{message} Consider adding it to #{env_file_path}.")
+  else
+    env_file_example_path = 'fastlane/example.env'
+    env_file_dir = File.dirname(env_file_path)
+    env_file_name = File.basename(env_file_path)
+
+    UI.user_error! <<~MSG
+      #{env_file_name} not found in #{env_file_dir}!
+
+      Please copy #{env_file_example_path} to #{env_file_path} and fill in the values for the automation you require.
+
+      mkdir -p #{env_file_dir} && cp #{env_file_example_path} #{env_file_path}
+    MSG
+  end
+end
