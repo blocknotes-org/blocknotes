8f35544e262aa83c1176822ea2102cfdadea4af8
Ella <ella@vandurpe.com>
2024-07-21 13:56:22 +0200

    Update start page

--- src/start.jsx
+++ src/start.jsx
@@ -4,69 +4,132 @@ import { Filesystem } from '@capacitor/filesystem';
 import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
+const info = `
+<!-- wp:paragraph -->
+<p>Notes are stored as simple files, so you own your data. Blocknotes never connects to the internet, so it’s completely offline and private. Files are stored natively, so you can use another service to sync them across devices (such as iCloud). It’s <a href="https://github.com/blocknotes-org/blocknotes">open source</a>.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>On iPhone, there’s a a <a href="https://apps.apple.com/us/app/blocknotes/id6450189974">native iOS app available</a>. Unfortunately no Android support yet.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>On Desktop you have four options:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item -->
+<li>Install the <a href="https://apps.apple.com/us/app/blocknotes/id6450189974">native app (MacOS only)</a>.</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Click “Pick Folder” on <a href="https://blocknotes.org">blocknotes.org</a> in Chrome, Edge, or any other Chromium based browser. Check <a href="https://caniuse.com/native-filesystem-api">browser support for the File System Access API</a>.</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>In Chrome, you can also <a href="https://support.google.com/chrome/answer/9658361">install</a> the app through <a href="https://blocknotes.org">blocknotes.org</a> as a PWA (Progressive Web App).</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>If you feel adventurous, you could <a href="https://github.com/blocknotes-org/blocknotes">build the app yourself</a>, and run it on a local web server.</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p>Features:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item -->
+<li>Tags, filtering, and search</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Revisions</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Dark mode</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Rich text editing</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Task lists</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p>Coming soon:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item -->
+<li>Local images</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Internal linking</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Document scanning</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Pinned notes</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>List of trashed items</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Restore revisions from the UI</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Different representations of the notes list</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Plugins</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+`;
+
 export default function Start({
 	setSelectedFolderURL,
 	canUseNativeFilesystem,
 }) {
 	return (
-		<main id="start">
-			<h1>Welcome to Blocknotes!</h1>
-			<p>
-				Blocknotes works completely offline, and notes are stored in the
-				folder you pick, so you have complete control over your data.
-			</p>
-			<ul>
-				<li>
-					There&apos;s a{' '}
-					<a href="https://apps.apple.com/us/app/blocknotes/id6450189974">
-						native iOS and macOS app available in the App Store.
-					</a>
-				</li>
-				<li>
-					You can also use the web version (button below), but it only
-					works in Chrome and and Chromium browsers (such as Edge,
-					Opera) right now, because it uses the native filesystem API.
-					It does <em>not</em> work in Safari and Firefox, and neither
-					does it work in <em>mobile</em> browsers, even Chrome.
-				</li>
-				<li>
-					In Chrome, you can also install it as a PWA (Progressive Web
-					App)!
-				</li>
-				<li>
-					You can also{' '}
-					<a href="https://github.com/blocknotes-org/blocknotes">
-						build the app yourself
-					</a>
-					, and run it on a local webserver.
-				</li>
-			</ul>
-			<p>Please pick a folder to read and write your notes.</p>
-			<p>
-				For the best experience, create a new cloud folder. If you use
-				the iOS/macOS app, it will store notes in a “Blocknotes” folder
-				within your iCloud folder.
-			</p>
-			<Button
-				variant="primary"
-				className="start-button"
-				// eslint-disable-next-line jsx-a11y/no-autofocus
-				autoFocus
-				disabled={!canUseNativeFilesystem}
-				onClick={async () => {
-					try {
-						const { url } = await Filesystem.pickDirectory();
-						setSelectedFolderURL(url);
-					} catch (e) {
-						// eslint-disable-next-line no-alert
-						window.alert(e.message);
-					}
-				}}
-			>
-				{canUseNativeFilesystem
-					? __('Pick Folder')
-					: __('Not available in this browser')}
-			</Button>
-		</main>
+		<div style={{ overflow: 'auto', height: '100%' }}>
+			<main id="start">
+				<h1>{__('Blocknotes')}</h1>
+				<Button
+					variant="primary"
+					className="start-button"
+					// eslint-disable-next-line jsx-a11y/no-autofocus
+					autoFocus
+					disabled={!canUseNativeFilesystem}
+					onClick={async () => {
+						try {
+							const { url } = await Filesystem.pickDirectory();
+							setSelectedFolderURL(url);
+						} catch (e) {
+							// eslint-disable-next-line no-alert
+							window.alert(e.message);
+						}
+					}}
+				>
+					{canUseNativeFilesystem
+						? __('Pick Folder')
+						: __('Not available in this browser')}
+				</Button>
+				<div dangerouslySetInnerHTML={{ __html: info }} />
+			</main>
+		</div>
 	);
 }
